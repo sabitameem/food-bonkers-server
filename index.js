c6ff599aa65c6f620a535e042470ff4407cad1c7
sabitameem <sabitameem578@gmail.com>
2023-05-04 16:08:18 +0530

    done

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-
-
 const chefData = require('./data/chefData.json')
 app.use(cors());
 app.get('/', (req, res) =>{
     res.send('Dragon is running')
 });
+
 
 app.get('/chefData', (req, res)=>{
     res.send(chefData)

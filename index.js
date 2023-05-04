cc216bc2c77c4b1d2d0b15da62fef7f6be7bbaae
sabitameem <sabitameem578@gmail.com>
2023-05-04 15:59:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chefData = require('./data/chefData.json')
+app.use(cors());
+app.get('/', (req, res) =>{
+    res.send('Dragon is running')
+});
+
+app.get('/chefData', (req, res)=>{
+    res.send(chefData)
+})
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
+})

444a7ab529579615ae1e314292a17c834cf27f7d
sabitameem <sabitameem578@gmail.com>
2023-05-04 17:40:51 +0530

    view details done

--- index.js
+++ index.js
@@ -14,6 +14,35 @@ app.get('/chefData', (req, res)=>{
     res.send(chefData)
 })
 
+// app.get('/chefData/:id/recipes', (req, res) => {
+//     const { id } = req.params;
+//     const chef = chefData.find(chef => chef.id == id);
+  
+//     if (!chef) {
+//       return res.status(404).json({ message: `Chef with ID ${id} not found` });
+//     }
+  
+//     const recipes = chef.recipes;
+//     res.json(recipes);
+//   });
+  app.get('/chefData/:id/recipes', (req,res)=>{
+    const { id } = req.params;
+    const chef = chefData.find(chef => chef.id == id);
+    if(!chef){
+        return res.status(404).json({message: `Chef with ID ${id} not found`});
+    }
+
+    const recipes = chef.recipes;
+    res.json(recipes);
+  })
+//   app.get('/chefData/:id/recipes', (req, res) => {
+//     const chefId = req.params.id;
+//     const chef = chefData.find(chef => chef.id === chefId);
+//     const chefRecipes = recipes.filter(recipe => recipe.chef_id === chefId);
+//     const chefDataWithRecipes = { chef, recipes: chefRecipes };
+//     res.send(chefDataWithRecipes);
+//   });
+
 
 app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`)
